9eed314b5ba2cdd4cdb2260f829f104ee6353387
Dennis Playdon <dennis9953@gmail.com>
2019-01-28 12:14:49 +1300

    Add logic to find next holiday

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Titles from "./components/Titles";
 import CountryInput from "./components/CountryInput";
+import Holidays from "./components/Holidays";
 
 var API_KEY = process.env.REACT_APP_API_KEY;
 
@@ -11,7 +12,7 @@ class App extends Component {
     holidays: undefined,
     error: undefined
   };
-  getWeather = async e => {
+  getHolidays = async e => {
     e.preventDefault();
     console.log("Getting weather");
     var country = e.target.elements.country.value;
@@ -19,10 +20,10 @@ class App extends Component {
       country = "NZ";
     }
     const api_call = await fetch(
-      `https://calendarific.com/api/v2/holidays?country=${country}&year=2018&api_key=${API_KEY}`
+      `https://calendarific.com/api/v2/holidays?country=${country}&year=2019&api_key=${API_KEY}`
     );
     var data = await api_call.json();
-    console.log(data);
+    console.log(data.response.holidays);
     if (data.response.length !== 0) {
       this.getNextHoliday(data.response.holidays);
     } else {
@@ -31,6 +32,32 @@ class App extends Component {
       });
     }
   };
+
+  getNextHoliday(holidays) {
+    var i;
+    var nextHoliday = holidays[0];
+    var todaysDate = new Date("2019-12-24");
+    console.log(`Today is ${todaysDate} and ${todaysDate < nextHoliday}`);
+    for (i = 1; i < holidays.length; i++) {
+      var holidayDate = new Date(holidays[i].date.iso);
+      if (
+        todaysDate < holidayDate &&
+        todaysDate > new Date(nextHoliday.date.iso)
+      ) {
+        nextHoliday = holidays[i];
+      } else if (todaysDate < holidayDate) {
+        var differenceInMilliseconds = Math.abs(todaysDate - holidayDate);
+        if (
+          differenceInMilliseconds <
+          Math.abs(todaysDate - new Date(nextHoliday.date.iso))
+        ) {
+          nextHoliday = holidays[i];
+        }
+      }
+    }
+    console.log("NEXT HOLIDAY");
+    console.log(nextHoliday);
+  }
   render() {
     return (
       <div>
@@ -42,7 +69,10 @@ class App extends Component {
                   <Titles />
                 </div>
                 <div className="col-xs-7 form-container">
-                  <CountryInput getWeather={this.getWeather} />
+                  <CountryInput getHolidays={this.getHolidays} />
+                  <Holidays holidays={this.state.holidays} />
+                  {/* holidays={this.state.holidays}
+                    error={this.state.error} */}
                 </div>
               </div>
             </div>
--- src/components/CountryInput.js
+++ src/components/CountryInput.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CountryInput = props => (
-  <form onSubmit={props.getWeather}>
+  <form onSubmit={props.getHolidays}>
     <input type="text" name="country" placeholder="Country..." />
     <button>Get Holiday</button>
   </form>
