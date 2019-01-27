0eb8a4bfdf882ce63d0527d345f2ca88ac1f6e54
Dennis Playdon <dennis9953@gmail.com>
2019-01-27 23:03:42 +1300

    Add Holidays component

--- src/App.js
+++ src/App.js
@@ -7,14 +7,29 @@ import CountryInput from "./components/CountryInput";
 var API_KEY = process.env.REACT_APP_API_KEY;
 
 class App extends Component {
+  state = {
+    holidays: undefined,
+    error: undefined
+  };
   getWeather = async e => {
     e.preventDefault();
     console.log("Getting weather");
+    var country = e.target.elements.country.value;
+    if (!country) {
+      country = "NZ";
+    }
     const api_call = await fetch(
-      `https://calendarific.com/api/v2/holidays?country=US&year=2018&api_key=${API_KEY}`
+      `https://calendarific.com/api/v2/holidays?country=${country}&year=2018&api_key=${API_KEY}`
     );
     var data = await api_call.json();
     console.log(data);
+    if (data.response.length !== 0) {
+      this.getNextHoliday(data.response.holidays);
+    } else {
+      this.setState({
+        error: "Please enter a valid country"
+      });
+    }
   };
   render() {
     return (
