da5f506b5326459a18b820900cfc6bc77396a281
Dennis Playdon <dennis9953@gmail.com>
2019-01-28 16:01:48 +1300

    Change button text to find holiday

--- src/components/CountryInput.js
+++ src/components/CountryInput.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const CountryInput = props => (
-  <form onSubmit={props.getHolidays}>
+  <form className="text-left" onSubmit={props.getHolidays}>
     <input type="text" name="country" placeholder="Country code..." />
-    <button>Get Holiday</button>
+    <button>Find Holiday</button>
   </form>
 );
 
