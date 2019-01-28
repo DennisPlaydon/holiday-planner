4445c2c96b014ffd1505a04615b78b0f91ad5fe8
Dennis Playdon <dennis9953@gmail.com>
2019-01-28 14:47:02 +1300

    Make input more explicit

--- src/components/CountryInput.js
+++ src/components/CountryInput.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const CountryInput = props => (
   <form onSubmit={props.getHolidays}>
-    <input type="text" name="country" placeholder="Country..." />
+    <input type="text" name="country" placeholder="Country code..." />
     <button>Get Holiday</button>
   </form>
 );
