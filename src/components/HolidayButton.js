5fdc47467999851e62a5a8faf80e289f509d3308
Dennis Playdon <dennis9953@gmail.com>
2019-01-28 17:23:11 +1300

    Add next holiday component

---
+++ src/components/HolidayButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HolidayButton = props => (
+  <div className="text-center">
+    <button onClick="">Next Holiday</button>
+  </div>
+);
+
+export default HolidayButton;
