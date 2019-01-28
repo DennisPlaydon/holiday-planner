36c5cffe580549deb3f1a22f32b46f61f5c68715
Dennis Playdon <dennis9953@gmail.com>
2019-01-28 17:48:04 +1300

    Updates

---
+++ precache-manifest.a6f905d7251b4110b96562a31162d590.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f634495d320c8a41109f8146c9cdb4b",
+    "url": "/holiday-planner/static/media/bg.4f634495.jpg"
+  },
+  {
+    "revision": "3969cb095ccd085f8c36",
+    "url": "/holiday-planner/static/js/runtime~main.3969cb09.js"
+  },
+  {
+    "revision": "9e96a1c13ae04548a5b2",
+    "url": "/holiday-planner/static/js/main.9e96a1c1.chunk.js"
+  },
+  {
+    "revision": "b3fd3f8cd7eea75553a2",
+    "url": "/holiday-planner/static/js/1.b3fd3f8c.chunk.js"
+  },
+  {
+    "revision": "9e96a1c13ae04548a5b2",
+    "url": "/holiday-planner/static/css/main.dfe877b9.chunk.css"
+  },
+  {
+    "revision": "b3fd3f8cd7eea75553a2",
+    "url": "/holiday-planner/static/css/1.bb41ba7d.chunk.css"
+  },
+  {
+    "revision": "71716bb7be67f16c8b12582e33934648",
+    "url": "/holiday-planner/index.html"
+  }
+];
