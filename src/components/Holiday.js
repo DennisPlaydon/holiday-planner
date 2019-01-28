8f95ca06d940c17b274174115720aedc2cce4b21
Dennis Playdon <dennis9953@gmail.com>
2019-01-28 13:29:58 +1300

    Display API response using holiday component

---
+++ src/components/Holiday.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Holiday = props => (
+  <div className="weather__info">
+    {props.holidayName && (
+      <p className="weather__key">
+        Name:
+        <span className="weather__value"> {props.holidayName}</span>
+      </p>
+    )}
+    {props.holidayDescription && (
+      <p className="weather__key">
+        Description:
+        <span className="weather__value"> {props.holidayDescription}</span>
+      </p>
+    )}
+    {props.daysUntil && (
+      <p className="weather__key">
+        Day until:
+        <span className="weather__value"> {Math.ceil(props.daysUntil)}</span>
+      </p>
+    )}
+  </div>
+);
+
+export default Holiday;
